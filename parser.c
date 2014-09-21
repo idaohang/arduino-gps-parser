0761c1d6ba8f7ae9001f29045c775b7dc00cdd6c
Adam Claassen <claassenadam@gmail.com>
2014-09-20 21:16:18 -0400

    Added func prototypes

---
+++ parser.c
@@ -0,0 +1,26 @@
+/* @author Adam Claassen, 2014 <claassenadam@gmail.com>
+* 
+*/
+
+#include <Serial.h>
+
+void Parser::Parser(SerialPort port){
+
+}
+
+void Parser::update(){
+
+}
+
+float Parser::time(){
+
+}
+
+float Parser::lat(){
+
+}
+
+float Parser::lon(){
+
+}
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+Basic NEMA GPS sentence parser. Gets latest data on update calls, and 
+makes it availabe through convenient functions.
